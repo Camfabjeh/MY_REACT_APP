41aa4ab29c9bc48fe067c07b134206fba9ab4c13
Camfabjeh <camille.fabry@yahoo.com>
2023-03-28 17:17:54 +0200

    first props

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
-  const monStyle = {
-    backgroundColor: "grey",
-    borderRadius: "10%",
-    textAlign: "center",
-    fontSize: "24px",
-  }
+  const pokemonList = [
+    {
+      name: "Bulbasaur",
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+    },
+    {
+      name: "mew",
+    },
+  ];
 
   return (
-    <div style={monStyle}>
-      <PokemonCard />
+    <div>
+      <PokemonCard pokemon={pokemonList[0]} />
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,28 +1,11 @@
-const pokemonList = [
-  {
-    name: "Bulbasaur",
-    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-  },
-  {
-    name: "mew",
-  },
-];
+function PokemonCard({ pokemon }) {
+  // const pokemon = pokemonList[1];
 
-//On a une variable qui est égale à un tableau d'objets
-//On veut que le return affiche l'imgSrc SEULEMENT si il y en a une, si undefined, afficher un p "???"
-
-// === "" ? { pokemon.imgSrc } : } 
-
-
-function PokemonCard() {
-  const pokemon = pokemonList[1];
-
-  return <div>
-    {pokemon.imgSrc ? (<img src={pokemon.imgSrc} />) : (<p>???</p>)}
+  return <div className="card">
+    {pokemon.imgSrc ? (
+      <img src={pokemon.imgSrc} className="card-img" />) : (<p>???</p>)}
     <figcaption>{pokemon.name}</figcaption>
   </div>
 }
-
-
 
 export default PokemonCard;
