97a6145d883a6f0cd8d953c975ef851fe5085865
Camfabjeh <camille.fabry@yahoo.com>
2023-03-28 09:45:38 +0200

    creation of the PokemonCard component

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,8 @@
+function PokemonCard() {
+  return <div>
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemon image" />
+    <figcaption>Bulbasaur</figcaption>
+  </div>
+}
+
+export default PokemonCard;
